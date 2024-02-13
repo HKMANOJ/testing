a91817f6fd9b5a27181ad17b63e45353a927c422
HKMANOJ <hkmanoj66@gmail.com>
2024-02-13 20:23:58 +0530

    first commit

---
+++ session.js
@@ -0,0 +1,22 @@
+// let firstnamae='manoj';
+// let secondname='hk';
+
+// console.log("wlcoome MR" +`${firstnamae}`);
+// console.log(2**3);
+// const myArr = ["Orange", "Banana", "Mango", "Kiwi"];
+// console.log(...myArr);
+
+//  let caller=roll_number=> console.log(`hwllo ${roll_number}`)
+
+// setTimeout(caller,5000,24)
+
+// function call(str){
+//     console.log(`hello ${str}`)
+// }
+// setTimeout(call,3000,"manoj")
+// const myArr = ["Orange", "Banana", "Mango", "Kiwi"];
+
+// myArr.forEach(function(str){
+//     console.log(str)
+// })
+console.log("jello")
